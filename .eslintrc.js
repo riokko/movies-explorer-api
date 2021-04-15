6885984c199878f8cb95367f528713d85a59db5d
Daria Barova <breya@yandex.ru>
2021-04-15 15:20:10 +0300

    add infrastructure files

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ["airbnb-base", "prettier"],
+    rules: {
+        "no-underscore-dangle": "off",
+        indent: ["error", 4],
+        quotes: [2, "double"],
+        "object-curly-newline": "off",
+        "prefer-arrow-callback": "error",
+    },
+};
+
+
